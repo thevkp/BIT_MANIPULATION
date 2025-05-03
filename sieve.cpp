51c827c9728b99f803c886b8dc6f2bd4872c1770
Vishal Patel <vishalkiranpatel@gmail.com>
2025-05-03 21:40:37 +0530

    Add sieve.cpp

---
+++ sieve.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <bitset>
+#include <vector>
+using namespace std;
+
+const int MAX = 1e6;
+bitset<MAX> isPrimes;
+vector<int> primes;
+
+
+void sieve(){
+  isPrimes.set();
+  isPrimes[0] = isPrimes[1] = false;
+
+  for(int i = 2; i * i <= MAX; i++){
+    if(isPrimes[i]){
+      for(int j = i * i; j <= MAX; j += i){
+        isPrimes[j] = false;
+      }
+    }
+  }
+
+  for(int i = 2; i <= MAX; i++){
+    if(isPrimes[i]){
+      primes.push_back(i);
+    }
+  }
+}
+
+
+
+int main(){
+
+  sieve();
+
+  // Number of test cases
+  int t;
+  cin >> t;
+  while(t--){
+
+    //First n prime numbers(inclusive)
+    int n;
+    cin >> n;
+
+    for(int prime : primes){
+      if(prime > n) break;
+      cout << prime << " ";
+    }
+    cout << endl;
+  }
+
+
+  return 0;
+}
